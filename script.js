f2834cdafb5b723b74778cefdc08df8e0b8f7eab
AadiKrishnatraya <aadi.25b01010018@abes.ac.in>
2026-02-24 09:39:14 +0530

    js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-console.log("hi");
-document.write("Hello")
+// let num=10;
+// console.log(num);
+// console.log(false);
+// console.log(typeof(num));
+// console.log("Hello World");
+
+// console.log(typeof(num));
+// num= "hi";
+// console.log(num);
+// console.log(typeof(num));
+// num= null;
+// console.log(num);
+// console.log(typeof(num));
+
+let sayHello= function(){
+    alert('Hello World');
+}
+var arr= [1,2,3];
+console.log(arr);
+var person={
+    name:'your name',
+    age:'51',
+
+
+}
+console.log(person.name)
