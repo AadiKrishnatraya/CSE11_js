20d581d2cc082269836ac21b6376ece0db2b3975
AadiKrishnatraya <aadi.25b01010018@abes.ac.in>
2026-03-10 10:20:35 +0530

    Dom

---
+++ dom.js
@@ -0,0 +1,19 @@
+// const result= document.getElementById("para");
+// console.log(result)
+// result.innerHTML= "<b>Hello</b>";
+// result.title= "myName"
+// // result.innerText= "<b>hi</b>";
+// const hi= document.getElementsByClassName("p1")
+// console.log(hi)
+// hi[0].innerHTML= "<b>Aadi</b>"
+// hi[1].innerHTML= "<b>Krishnatraya</b>"
+
+// const tag= document.getElementsByTagName("p");
+// console.log(tag);
+// tag[1].innerHTML= "My para";
+// tag[1].style.color= "blue"
+
+// querySelector only get firts element 
+const test= document.querySelectorAll("p");
+test[1].innerHTML= "Hello"
+test[1].style.color= "red";
